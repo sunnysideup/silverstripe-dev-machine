e7594b2c3256978c0f38016b6273ab47077af780
Nicolaas / Sunn Side Up <github@sunnysideup.co.nz>
2022-02-21 20:36:19 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -145,7 +145,14 @@ nano ~/.bashrc
 # reload ~/.bashrc
 source ~/.bashrc
 
-
+sudo apt install php-cli unzip
+cd ~
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+echo $HASH
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer -v
 # install linter
 composer global require sunnysideup/easy-coding-standards:dev-master
 composer global update
