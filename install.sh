0e9c3f23f2a2cd27c15e8d7397f8885e1c71e671
SSUOffice <office@sunnysideup.co.nz>
2024-11-01 12:04:11 +1300

    FIX: Clean up script comments to be consistent

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
+# Update to latest version of ubuntu.
+# https://ubuntu.com/tutorials/upgrading-ubuntu-desktop#1-before-you-start
 
-# update to latest version of ubuntu.
-https://ubuntu.com/tutorials/upgrading-ubuntu-desktop#1-before-you-start
 
-
-# allow ip4:
-https://askubuntu.com/questions/1123177/sudo-add-apt-repository-hangs
+# Allow ip4:
+# https://askubuntu.com/questions/1123177/sudo-add-apt-repository-hangs
 
 
 #########################################
 # BASICS
 #########################################
 
-
-
-# get latest and greatest
+# Get latest and greatest
 sudo apt -y update
 
 #########################################
@@ -38,34 +35,35 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/v
 sudo apt -y update && sudo apt -y install code
 
 
-# install chrome
-https://www.google.com/chrome/
+# Install chrome
+# https://www.google.com/chrome/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt -y install ./google-chrome-stable_current_amd64.deb
 
-# install git
+# Install git
 sudo apt -y install git
 git config --global corsudo e.filemode false
 
-# install meld - for git merges
+# Install meld - for git merges
 sudo apt -y install meld
 	
-# install curl
+# Install curl
 sudo apt -y install curl
 
 
-# install gimp
-# see: 
+# Install gimp
+ 
 # Uninstall GIMP
 sudo apt-get -y autoremove gimp gimp-plugin-registry
+
 # Add the following PPA
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp
 sudo apt -y update
+
 # Reinstall the latest GIMP
 sudo apt -y install gimp
 
-# or ...
-sudo snap install gimp --channel=edge
+# Or... sudo snap install gimp --channel=edge
 
 
 # # atom
@@ -79,17 +77,17 @@ sudo snap install gimp --channel=edge
 
 
 
-# install npm
+# Install npm
 sudo apt -y install nodejs
 sudo apt -y install npm
 
-# install nvm
+# Install nvm
 # https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-# install slack
+# Install slack
 sudo snap install slack --classic
 
 
@@ -97,10 +95,10 @@ sudo snap install slack --classic
 # LAMP
 #########################################
 
-# install apache
+# Install apache
 sudo apt -y install apache2
 	
-# install mysql
+# Install mysql
 sudo apt -y  install mysql-server
 # VALIDATE PASSWORD COMPONENT :n
 # Remove anonymous users?: n
@@ -109,39 +107,43 @@ sudo apt -y  install mysql-server
 # Reload privilege tables now?: y
 
 sudo /usr/bin/mysql_secure_installation
+
 # https://stackoverflow.com/questions/39281594/error-1698-28000-access-denied-for-user-rootlocalhost
-# login as root to mysql then ... 
+# Login as root to mysql then ... 
+echo "Please run the below commands in mysql"
 sudo mysql -u root -p
-mysql> flush privileges;
-mysql> use mysql;
-mysql> UNINSTALL COMPONENT 'file://component_validate_password';
-mysql> ALTER USER 'root'@'localhost' IDENTIFIED BY '';
-mysql> UPDATE user SET plugin="mysql_native_password" WHERE User='root';
-mysql> quit;
+
+# Run these commands
+# mysql> flush privileges;
+# mysql> use mysql;
+# mysql> UNINSTALL COMPONENT 'file://component_validate_password';
+# mysql> ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+# mysql> UPDATE user SET plugin="mysql_native_password" WHERE User='root';
+# mysql> quit;
 
 sudo pkill mysqld
 sudo service mysql restart
 
 # mysql -u root -p[as you set it]
-# see: https://linuxconfig.org/how-to-reset-root-mysql-password-on-ubuntu-18-04-bionic-beaver-linux
+# See: https://linuxconfig.org/how-to-reset-root-mysql-password-on-ubuntu-18-04-bionic-beaver-linux
 
-# install additioanl apache modules
+# Install additioanl apache modules
 sudo a2enmod rewrite
 sudo a2enmod vhost_alias
 sudo a2enmod proxy
 sudo a2enmod proxy_http
 sudo service apache2 restart
 
-# install phpmyadmin
+# Install phpmyadmin
 sudo apt -y install phpmyadmin	
 
-# install php versions
+# Install php versions
 git clone https://github.com/sunnysideup/silverstripe-switch-php-versions.git
 
-# install
+# Install
 sudo bash silverstripe-switch-php-versions/install.sh
 
-# remove install files
+# Remove install files
 rm silverstripe-switch-php-versions -rf
 
 
@@ -149,7 +151,7 @@ rm silverstripe-switch-php-versions -rf
 # COMPOSER
 #########################################
 
-# install composer
+# Install composer
 sudo apt -y install composer
 nano ~/.bashrc 
 # add one of these: 
@@ -160,7 +162,7 @@ source ~/.bashrc
 #OR
 echo "- PATH=~/.config/composer/vendor/bin:$PATH" >> ~/.bashrc
 
-#https://getcomposer.org/download/
+# https://getcomposer.org/download/
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
@@ -168,7 +170,8 @@ php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
 
 composer -v
-# install linter
+
+# Install linter
 composer global require sunnysideup/easy-coding-standards:dev-master
 composer global update
 
@@ -177,17 +180,16 @@ composer global update
 # SETTINGS
 #########################################
 
-# set up public key and create aliases
+# Set up public key and create aliases
 sudo nano ~/.ssh/config 
 	
 
-# set php settings
+# Set php settings
 sudo echo "date.timezone = 'Pacific/Auckland'" >> /etc/php/7.4/apache2/php.ini
 sudo service apache2 restart
 
-# set up public key
+# Set up public key
 ssh-keygen
-
 
 
 #########################################
@@ -195,34 +197,35 @@ ssh-keygen
 #########################################
 
 
-
-# set up sites-enables - see default.conf
+# Set up sites-enables - see default.conf
 sudo nano /etc/apache2/sites-enabled/sites-enabled.conf
 
 sudo chown ssu:www-data . -R
-# create folders for silverstripe
+
+# Create folders for silverstripe
 mkdir /var/www/ss3
 mkdir /var/www/ss4
 mkdir /var/www/ss5
 
-# set up aliases
+# Set up aliases
 sudo ln -s /var/www/ss3 /ss3
 sudo ln -s /var/www/ss4 /ss4
 sudo ln -s /var/www/ss5 /ss5
 
-# sspak
+# SSPAK
 cd /usr/local/bin
 curl -sS https://silverstripe.github.io/sspak/install | sudo php
 
 
-# test up test site. 
+# Test up test site. 
 cd /var/www/ss4
 echo "127.0.0.1 test.ss4" | sudo tee -a /etc/hosts
 sudo chown www-data /var/www/ss4 -R #must be all www-data
 composer create-project silverstripe/installer test 4.7
 cd test
 ls
-# install sake
+
+# Install sake
 cd /var/www/ss4/test
 sudo ./vendor/bin/sake installsake
 
